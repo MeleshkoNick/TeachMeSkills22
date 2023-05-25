aac5e0f34d0f8bd498836c302eea8fe51eb16eb9
Nicholas Meleshko <nicholasmeleshko@gmail.com>
2023-05-25 11:30:32 +0300

    hw4 ex3

---
+++ src/Even.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Even {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your number: ");
+        int number = scanner.nextInt();
+        System.out.println(even(number));
+    }
+
+    public static String even(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+}
+

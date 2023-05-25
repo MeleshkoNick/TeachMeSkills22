ddc912d4991da7389c4055d79c2fae4ccdd5f308
Nicholas Meleshko <nicholasmeleshko@gmail.com>
2023-05-25 19:23:08 +0300

    HW ex4

---
+++ src/Temperature.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Temperature {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your temperature: ");
+        int temperature = scanner.nextInt();
+        System.out.println("Your temperature: " +yourTemperature(temperature));
+    }
+    public static String yourTemperature(int temperature) {
+        if (temperature > -5) {
+            return  "Warm";
+        } else if (temperature > -20 && temperature <= -5) {
+            return "Normal";
+        } else {
+             return "Cold";
+        }
+    }
+}

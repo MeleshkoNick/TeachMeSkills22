69a0dfd25b99830ffaf99fd9afac6412073a41e8
Nicholas Meleshko <nicholasmeleshko@gmail.com>
2023-05-26 14:56:27 +0300

    fix

--- src/Decrement.java
+++ src/Decrement.java
@@ -1,12 +1,9 @@
 public class Decrement {
     public static void main(String[] args) {
         number(); }
-
     public static void number() {
         for (int i=5 ;i >= 1 ; i-- ){
             System.out.println(i);
-
         }
-
     }
 }

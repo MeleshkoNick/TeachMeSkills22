557f35f3ba3912ce1bd2cc5980984d103be7d6d0
Nicholas Meleshko <nicholasmeleshko@gmail.com>
2023-05-26 13:43:42 +0300

    hw4 ex1

---
+++ src/LoopEvenOdd.java
@@ -0,0 +1,12 @@
+public class LoopEvenOdd {
+    public static void main(String[] args) {
+        odd(); }
+    public static void odd() {
+        for(int i = 0;i<=100;i++){
+            if (i %2 == 1) {
+                System.out.println(i);
+            }
+            }
+
+    }
+}

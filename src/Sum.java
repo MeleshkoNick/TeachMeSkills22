f0f172e8f7eb51a9cd01f6060a36de4095cb5ae0
Nicholas Meleshko <nicholasmeleshko@gmail.com>
2023-05-26 19:14:47 +0300

    hw ex3

--- src/Sum.java
+++ src/Sum.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
-
 public class Sum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your number: ");
         int number = scanner.nextInt();
-        sum(number); }
-    public static void sum(int number) {
-        for (int i = 1; i < number; i++)
-            System.out.println(i);
+        System.out.println(getSum(number)); }
+    public static int getSum(int number) {
+        int sum = 0;
+        for (int i = 1; i < number; i++){
+        sum += i; }
+        return sum;
     }
 }

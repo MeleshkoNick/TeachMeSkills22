0cabe788895f25d43c2c26c86863efe03e6b3bc6
Nicholas Meleshko <nicholasmeleshko@gmail.com>
2023-05-27 09:19:05 +0300

    hw ex4

---
+++ src/While.java
@@ -0,0 +1,10 @@
+public class While {
+    public static void main(String[] args) {
+        int number = 0;
+        while (number<98){
+            number += 7;
+            System.out.print(number + " ");
+        }
+    }
+
+}

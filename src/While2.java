6ff4e55bf9a69f3f32e53f25f900f0f689785613
Nicholas Meleshko <nicholasmeleshko@gmail.com>
2023-05-27 09:30:14 +0300

    hw ex5

---
+++ src/While2.java
@@ -0,0 +1,9 @@
+public class While2 {
+    public static void main(String[] args) {
+        int number = 0;
+        for (int i = 0; i < 10; i++) {
+            System.out.print(number + " ");
+            number -= 5;
+        }
+    }
+}
